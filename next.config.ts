ff223de4ff88053f2b60d8f27bd1dc120512cc6f
Soliu Sulyman <solihullahsulyman@gmail.com>
2026-09-05 14:46:56 +0100

    fix: switch build script to webpack to prevent turbopack out of memory crash

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,29 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   reactCompiler: true,
+// };
+
+// export default nextConfig;
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  productionBrowserSourceMaps: false,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  webpack: (config) => {
+    // Limits cache and memory consumption during build processing
+    config.performance = {
+      ...config.performance,
+      hints: false,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
